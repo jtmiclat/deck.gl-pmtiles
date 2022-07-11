28056888e936981b779e37866b5cfb732665588a
Jt Miclat <jtmiclat@gmail.com>
2022-07-11 23:58:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Deck GL Layer for PMtiles
+
+## TODDO
+
+- [x] Get it to work
+- [ ] Build simple example
+- [ ] Convert to typescript
+- [ ] Tests
+- [ ] Deploy as package
---
+++ index.js
@@ -0,0 +1,49 @@
+import { MVTLayer } from "deck.gl";
+import { PMTiles } from "pmtiles";
+
+export default class PMTilesLayer extends MVTLayer {
+  async _updateTileData() {
+    let data = this.props.data;
+    let tileJSON = null;
+    let pmtiles = new PMTiles(data);
+    this.setState({ data, tileJSON, pmtiles });
+  }
+  getTileData(loadProps) {
+    const { data, binary, pmtiles } = this.state;
+    const { index, signal } = loadProps;
+    const { x, y, z } = index;
+    let loadOptions = this.getLoadOptions();
+    const { fetch } = this.props;
+    const zxyPromise = pmtiles.getZxy(z, x, y);
+    return zxyPromise.then((val) => {
+      if (!val) {
+        return null;
+      }
+      loadOptions = {
+        ...loadOptions,
+        mimeType: "application/x-protobuf",
+        mvt: {
+          ...loadOptions?.mvt,
+          coordinates: this.context.viewport.resolution ? "wgs84" : "local",
+          tileIndex: index,
+          // Local worker debug
+          // workerUrl: `modules/mvt/dist/mvt-loader.worker.js`
+          // Set worker to null to skip web workers
+          // workerUrl: null
+        },
+        gis: binary ? { format: "binary" } : {},
+        fetch: {
+          headers: {
+            Range: "bytes=" + val.offset + "-" + (val.offset + val.length - 1),
+          },
+        },
+      };
+      return fetch(data, {
+        propName: "data",
+        layer: this,
+        loadOptions,
+        signal,
+      });
+    });
+  }
+}
